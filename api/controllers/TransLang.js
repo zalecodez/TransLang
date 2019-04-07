7978a6adcc2ec19c56430c339774e3b998a02fda
Zale Young <zyoung@middlebury.edu>
2019-04-07 02:22:50 -0400

    fix async spawn order

--- api/controllers/TransLang.js
+++ api/controllers/TransLang.js
@@ -42,65 +42,67 @@ exports.translate = function(req, res){
 };
 
 exports.translateV2 = function(req,res){
-    var input = req.file;
-    console.log(req.body);
-    console.log(input);
+  var input = req.file;
+  console.log(req.body);
+  console.log(input);
 
-    if(!input){
-        return Helper.send400(res, "Usage: /api/translate?text=sentence to translate");
+  if(!input){
+    return Helper.send400(res, "Usage: /api/translate?text=sentence to translate");
+  }
+
+
+
+  const inputPath = path.join(__dirname, '../../', input.path);
+  const newInputPath = path.join(path.dirname(inputPath), 'in.flac');
+
+  console.log(`About to call ffmpeg -i ${inputPath} -f flac ${newInputPath}`);
+  const convertProcess = spawn('ffmpeg', ['-i', inputPath, '-f', 'flac', newInputPath], {cwd: __dirname});
+  convertProcess.on('error', function(err){
+    console.log(err);
+  });
+
+  convertProcess.on('close', function(code){
+    console.log('Convert Exit Code', code);
+    if(code != 0){
+      Helper.send500(res, "File Conversion Error");
     }
+    else{
 
+      console.log(`About to call python TransLang_v2.py ${newInputPath}`);
+      const pythonProcess = spawn('python', ['../../TransLang_v2.py', newInputPath], {cwd: __dirname});
 
-
-    const inputPath = path.join(__dirname, '../../', input.path);
-    const newInputPath = path.join(path.dirname(inputPath), 'in.flac');
-
-    console.log(`About to call ffmpeg -i ${inputPath} -f flac ${newInputPath}`);
-    const convertProcess = spawn('ffmpeg', ['-i', inputPath, '-f', 'flac', newInputPath], {cwd: __dirname});
-    convertProcess.on('error', function(err){
-      console.log(err);
-    });
-
-    convertProcess.on('close', function(code){
-      console.log('Convert Exit Code', code);
-      if(code != 0){
-        Helper.send500(res, "File Conversion Error");
-      }
-      fs.unlink(inputPath, (err) => {
-        if(err) throw err;
-        console.log('old input file deleted');
-      });
-    });
-        
-    console.log(`About to call python TransLang_v2.py ${newInputPath}`);
-    const pythonProcess = spawn('python', ['../../TransLang_v2.py', newInputPath], {cwd: __dirname});
-
-    pythonProcess.stdout.on('data', (data) => {
+      pythonProcess.stdout.on('data', (data) => {
         console.log(`Output: ${data}`);
-    });
+      });
 
-    pythonProcess.stderr.on('data', (data) => {
+      pythonProcess.stderr.on('data', (data) => {
         console.log(`Error: ${data}`);
-    });
+      });
 
-    pythonProcess.on('error', function(err){
+      pythonProcess.on('error', function(err){
         console.log(err);
-    });
+      });
 
-    pythonProcess.on('close', function(code){
+      pythonProcess.on('close', function(code){
         console.log(`Exit Code: ${code}`);
         if(code == 0){
-            res.redirect('/output.mp3');
-            //res.status(200).sendFile(path.join(__dirname, '../../public', 'output.mp3'))
+          res.redirect('/output.mp3');
+          //res.status(200).sendFile(path.join(__dirname, '../../public', 'output.mp3'))
         }
         else{
-            Helper.send500(res, "Internal Server Error");
+          Helper.send500(res, "Internal Server Error");
         }
         fs.unlink(newInputPath, (err) => {
-            if(err) throw err;
-            console.log('input file deleted');
+          if(err) throw err;
+          console.log('input file deleted');
         });
+      });
+    }
+    fs.unlink(inputPath, (err) => {
+      if(err) throw err;
+      console.log('old input file deleted');
     });
+  });
 
 };
 
