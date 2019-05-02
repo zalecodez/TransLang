cbdf767aaa7df95ab3b35ffaab4deced76583b0a
Zale Young <zyoung@middlebury.edu>
2019-05-02 02:45:57 -0400

    add socket pings

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
+//const app = require('express')();
+//const http = require('http').createServer(app);
+//const io = require('socket.io')(http);
+//
+//app.get('/', function(req, res){
+//  res.sendfile('index.html');
+//});
+//
+//io.on('connection', function(socket){
+//  console.log('a user connected');
+//});
+//
+//setInterval(() => {
+//  console.log('pinging');
+//  io.emit('ping', { data: (new Date())/1});
+//}, 1000);
+//
+//http.listen(3000, function(){
+//  console.log('listening on *:3000');
+//});
+
+
+
 require('dotenv').config();
 
 var express = require('express'),
     server = express(),
+    http = require('http').Server(server),
     port = process.env.PORT || 3000,
     bodyParser = require('body-parser'),
-    Helper = require('./api/controllers/helper');
+    Helper = require('./api/controllers/helper'),
+    io = require('socket.io')(http);
 
 server.use(express.static('public'));
 
@@ -18,7 +43,15 @@ server.use(function(req, res){
     Helper.send404(res, "You Lost Bro");
 });
 
-server.listen(port);
-console.log("TransLang API Server started on port " + port);
 
+io.on('connection', (socket) => {
+  console.log('user connected');
+});
 
+setInterval(() => {
+  io.emit('ping', {data: (new Date())/1});
+}, 1000);
+
+http.listen(port, () => {
+  console.log("TransLang API Server started on port " + port);
+});
